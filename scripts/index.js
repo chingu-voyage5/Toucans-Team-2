f399fa9ae9d4bfc021ab8d6ca258291a40a0862a
ZumDeWald <zumdewald@gmail.com>
2018-06-02 22:53:28 -0500

    Now using iFrames for loading games
    Removed dead code
    Added iFrame for Memory Match Game
    Added JS to open game on title click

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
 $(document).ready(() => {
 
     /* VARIABLES */
+ let gameShowing = false;
 
+
+ const $gameTitle = $(".game-title");
 
 
 
     /* FUNCTIONS */
 
+    //Toggle Games show/hide
+    const gameShow = (target) => {
+      target.toggleClass("game-area-show");
+      target.toggleClass("game-area-hide");
+    }
+
+    //Opens Game Area
+    $(".game-container").on("click", ".game-title", (e) => {
+      if (!gameShowing) {
+        gameShow($(e.target).next());
+        gameShowing = true;
+      }
+    });
 
 
 
