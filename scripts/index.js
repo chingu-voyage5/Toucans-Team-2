1e0652df760039f2d8b779085ac0094a84f4e1d3
ZumDeWald <zumdewald@gmail.com>
2018-06-06 22:29:54 -0500

    Initial Styles Added
    Background textures and colors added
    Carets added next to Game Titles
    Carets change when Title is clicked

--- scripts/index.js
+++ scripts/index.js
@@ -16,9 +16,16 @@ $(document).ready(() => {
       target.toggleClass("game-area-hide");
     }
 
+    //Toggle Caret right / down
+    const caretChange = (target) => {
+      target.toggleClass("fas fa-caret-right");
+      target.toggleClass("fas fa-caret-down");
+    }
+
     //Opens Game Area
     $(".game-container").on("click", ".game-title", (e) => {
       gameShow($(e.target).next());
+      caretChange($(e.target).children());
       gameShowing = true;
     });
 
